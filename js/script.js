237a2912c492009c5e88323b2bdb060e50114f8f
asdfsdfdsfsd <asdfsdfsa@example.com>
2025-09-24 14:26:20 -0700

    First round

---
+++ js/script.js
@@ -0,0 +1,45 @@
+document.write("hi")
+
+// STEP 1 
+
+someMonth
+theMonth
+currentMonth
+summerMonth
+myLibraryFunction
+
+// STEP 2
+
+5.7
+'I am a string literal expression'
+true
+null
+
+// STEP 3 
+
+myVariableX = 7 * 5
+myOtherVariable = 50 + 9 + 240
+
+// STEP 4 
+
+let strFirstName
+let strLastName
+let strAddress
+let strCity
+let strState
+let intZipCode
+let intYourAge
+let strReferralSource
+let blnMayWeContactYou
+
+// STEP 5 
+
+let strFirstName = Shannon
+
+let strLastName
+strLastName = Bee
+
+cont intZipCode = 92104
+alert (intZipCode)
+
+// STEP 6 
