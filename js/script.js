56ccdd424aa1803cdac15e08705491b490ac4169
asdfsdfdsfsd <asdfsdfsa@example.com>
2025-09-24 14:54:58 -0700

    more changes

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ document.write("hi")
 
 // STEP 1 
 
-someMonth
-theMonth
-currentMonth
-summerMonth
-myLibraryFunction
+let someMonth
+let theMonth
+let currentMonth
+let summerMonth
+let myLibraryFunction
 
 // STEP 2
 
@@ -27,19 +27,59 @@ let strLastName
 let strAddress
 let strCity
 let strState
-let intZipCode
+// intZipCode
 let intYourAge
 let strReferralSource
 let blnMayWeContactYou
 
 // STEP 5 
 
-let strFirstName = Shannon
+strFirstName = 'Shannon'
 
-let strLastName
-strLastName = Bee
+// let strLastName
+strLastName = 'Bee'
 
-cont intZipCode = 92104
-alert (intZipCode)
+const intZipCode = 92104
 
 // STEP 6 
+
+let intMyNumber = 17
+let strMyString = 'I love coffee'
+
+console.log(intMyNumber)
+console.log(strMyString)
+
+// STEP 7
+
+blnIsItTrue = true
+strHelloString = 'Hi I am a string'
+
+let myFirstVariable = (blnIsItTrue + " " + strHelloString)
+console.log(myFirstVariable)
+
+blnIsItFalse = false
+intHiNumber = 45
+
+let mySecondVariable = (blnIsItFalse + " " + intHiNumber)
+console.log(mySecondVariable)
+
+// STEP 8
+
+// No the string literal is not valid, it needs \ before the first and last ""s, and \\\ before the apostrophe in order to print the string correctly.
+let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\\\'m not sure about the former.\"'
+console.log(someString)
+
+// STEP 9
+
+let iAmNull = null
+console.log(iAmNull)
+
+// STEP 10
+
+let unaryStr = 'Hello World'
+
+console.log()
+
+// STEP 11
+
+alert("Hello Zak Ruvalcaba," + " " + "welcome to the JavaScript class!")
