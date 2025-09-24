1d043127b726b7c9f737d8f59b8c4585f3ce94d9
asdfsdfdsfsd <asdfsdfsa@example.com>
2025-09-24 15:05:37 -0700

    almost done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
-document.write("hi")
-
 // STEP 1 
-
 let someMonth
 let theMonth
 let currentMonth
@@ -9,19 +6,16 @@ let summerMonth
 let myLibraryFunction
 
 // STEP 2
-
 5.7
 'I am a string literal expression'
 true
 null
 
 // STEP 3 
-
 myVariableX = 7 * 5
 myOtherVariable = 50 + 9 + 240
 
 // STEP 4 
-
 let strFirstName
 let strLastName
 let strAddress
@@ -33,53 +27,60 @@ let strReferralSource
 let blnMayWeContactYou
 
 // STEP 5 
-
 strFirstName = 'Shannon'
-
 // let strLastName
 strLastName = 'Bee'
-
 const intZipCode = 92104
 
 // STEP 6 
-
 let intMyNumber = 17
 let strMyString = 'I love coffee'
-
 console.log(intMyNumber)
 console.log(strMyString)
 
 // STEP 7
-
 blnIsItTrue = true
 strHelloString = 'Hi I am a string'
-
 let myFirstVariable = (blnIsItTrue + " " + strHelloString)
 console.log(myFirstVariable)
-
 blnIsItFalse = false
 intHiNumber = 45
-
 let mySecondVariable = (blnIsItFalse + " " + intHiNumber)
 console.log(mySecondVariable)
 
 // STEP 8
-
 // No the string literal is not valid, it needs \ before the first and last ""s, and \\\ before the apostrophe in order to print the string correctly.
 let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\\\'m not sure about the former.\"'
 console.log(someString)
 
 // STEP 9
-
 let iAmNull = null
 console.log(iAmNull)
 
 // STEP 10
-
 let unaryStr = 'Hello World'
-
 console.log()
 
 // STEP 11
+let firstPart = 'Hello Shannon Bee,'
+let secondPart = 'welcome to the JavaScript class!'
+alert(firstPart + ' ' + secondPart)
 
-alert("Hello Zak Ruvalcaba," + " " + "welcome to the JavaScript class!")
+// STEP 12
+let name = 'Shannon Bee'
+alert('Hello ${name}, welcome to the JavaScript class!')
+
+// STEP 13
+let course = 'JavaScript'
+alert('Hello ${name}, welcome to the JavaScript ${course)!')
+
+//STEP 14
+
+//STEP 15
+//STEP 16
+//STEP 17
+//STEP 18
+//STEP 19
+//STEP 20
+//STEP 21
+//STEP 22
